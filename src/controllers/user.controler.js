fba003db1997056062183bf27338877d3bb3df28
ibsharakrami <91590546+ibsharakrami@users.noreply.github.com>
2024-01-28 19:33:29 +0530

    logic Register Controller

--- src/controllers/user.controler.js
+++ src/controllers/user.controler.js
@@ -1,11 +1,45 @@
-import { asyncHandle } from "../utils/asyncHandle.js"
+import { asyncHandle } from "../utils/asyncHandle.js";
+import {ApiError} from "../utils/ApiError.js"
+import { User } from "../models/user.model.js"
+import { uploadOnCloundinary } from "../utils/cloudnary.js"
+import { upload } from "../middlewares/multer.middleware.js";
 
 
 const registerUser = asyncHandle( async (req , res) => {
-   res.status(200).json({
-    message : "pakka salte ashe",
+    const {fullName , email , username , password} = req.body
+    console.log("Fullname==>",fullName)
 
-  })
+    if (
+      [fullName , email, username, password].some((field)=>
+    field?.trim() === "")
+    ) {
+      throw new ApiError(400,"All Field is required")
+    }
+
+    const existUser = User.findOne({
+      $or: [{username} ,  {email}]
+    })
+
+    if(existUser) {
+      throw new ApiError(409 , "User with email or username already exists")
+    }
+
+    const avatarLocalPath =  req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath) {
+      throw new ApiError(400 ,"Avatar file is required")
+    }
+
+
+    const avatar = await uploadOnCloundinary(avatarLocalPath);
+    const coverImage = await uploadOnCloundinary(coverImageLocalPath);
+
+    if(!avatar) {
+      throw new ApiError(400 , "Avatar file is required")
+    }
+
+
 })
 
 export {registerUser}
